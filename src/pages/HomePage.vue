626605097b530aa2005573c0f98988deda6df6d5
tonlandoa <tonlandias@outlook.com>
2025-06-11 17:02:52 +0300

    Update For 11.06.2025-17-02

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -313,7 +313,7 @@ async function confirmBuy() {
                 <div class="nft-text-block">
                     <h2 class="nft-title">{{ $t('nft_unique_title') }}</h2>
                     <p class="nft-subtitle">{{ $t('nft_unique_subtitle') }}</p>
-                    <a href="/mint" class="nft-mint-btn">
+                    <a class="nft-mint-btn sold-out">
 
                         {{ $t('start_mint') }}
                     </a>
